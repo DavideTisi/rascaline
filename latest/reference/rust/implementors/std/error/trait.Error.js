52c9143b5239369566f55d69532190978ca14fcc
DavideTisi <DavideTisi@users.noreply.github.com>
2022-08-19 12:36:36 +0000

    deploy: a685634d39abcfb03814110c3a3352cb402b8543

---
+++ latest/reference/rust/implementors/std/error/trait.Error.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["equistore"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.63.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"equistore/enum.Error.html\" title=\"enum equistore::Error\">Error</a>","synthetic":false,"types":["equistore::Error"]}];
+implementors["rascaline"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.63.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rascaline/enum.Error.html\" title=\"enum rascaline::Error\">Error</a>","synthetic":false,"types":["rascaline::errors::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ latest/reference/rust/rascaline/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["Matrix3","A 3x3 square matrix type."],["Vector3D","A 3-dimensional vector type"]]};
